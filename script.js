38f9c0cdc0a7d4ce12b38b8546eb913d17b20623
Isfaq <sabbirhossainsh04@gmail.com>
2023-03-23 20:56:08 +0600

    Add backSpace()
    backSpace function works as a backspace when backBtn is clicked

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let operationButtons = document.querySelectorAll('.operationBtn');
 let display = document.querySelector('input[type="text"]');
 let equalBtn = document.querySelector('.equalBtn');
 let clearBtn = document.querySelector('.clearBtn');
+let backBtn = document.querySelector('.backBtn');
 display.value = '';
 
 //functions
@@ -31,7 +32,8 @@ const operate = function (num1, operator, num2) {
 };
 
 const updateDisplay = function (e) {
-  display.value = add(display.value, e.target.value); //string concat
+  if (display.value.length > 12) alert('Max number limit reached!');
+  else display.value = add(display.value, e.target.value); //string concat
 };
 
 const operation = function (e) {
@@ -51,16 +53,23 @@ const operation = function (e) {
   }
 };
 
-const showResult = function () {
-  if (display.value === '') alert('Please enter a digit!');
+const showResult = function (e) {
+  if (display.value === '')
+    alert('Please enter a digit!'); //prevents unusual '=' keypress
+  else if (num1 === '') alert('Please enter a operator!');
   else {
     num2 = Number(display.value);
     display.value = operate(num1, operator, num2);
+    clearCalculator(e);
   }
 };
 
-const clearCalculator = function () {
-  display.value = '';
+const backSpace = function () {
+  display.value = display.value.slice(0, -1);
+};
+
+const clearCalculator = function (e) {
+  if (e.target.value !== '=') display.value = '';
 
   //reset everything
   num1 = '';
@@ -82,3 +91,4 @@ operationButtons.forEach((button) => {
 });
 clearBtn.addEventListener('click', clearCalculator);
 equalBtn.addEventListener('click', showResult);
+backBtn.addEventListener('click', backSpace);
